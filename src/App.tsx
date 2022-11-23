2ab762c9218dd7772c6f4bffd242913b6c1391cc
Yevhen <shershen.yevhen@gmai>
2022-11-23 14:52:31 +0100

    Передача пропсов TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Card from "./components/Card";
 function App() {
-  return <div className="App">Work</div>;
+  return (
+    <div className="App">
+      <Card width="200px" height="200px" />
+    </div>
+  );
 }
 
 export default App;
